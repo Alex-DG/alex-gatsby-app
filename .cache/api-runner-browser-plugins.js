0e5ac15daab17e7ff2cb9880c2138b22f0efa791
Alexandre Di Guida <alexandre.diguida@gmail.com>
2020-12-05 17:34:22 +0000

    🎉 Begin project with Gatsby

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"G-XE84EHME5K","head":false,"anonymize":false,"respectDNT":false,"exclude":[],"pageTransitionDelay":0},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-XE84EHME5K",
+      },
+    },
+    "gatsby-plugin-offline",
+    "gatsby-plugin-typescript"
+  ],
+};
