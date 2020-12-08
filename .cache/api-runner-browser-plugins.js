a487d855029a09e544b308d42017ee0794c270e3
Alexandre Di Guida <alexandre.diguida@gmail.com>
2020-12-08 10:32:39 +0000

    🔥 Remove cache folder

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"G-XE84EHME5K","head":false,"anonymize":false,"respectDNT":false,"exclude":[],"pageTransitionDelay":0},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- src/components/Particles/index.tsx
+++ src/components/Particles/index.tsx
@@ -6,63 +6,13 @@ import useWindowSize from '../../config/hooks/useWindowSize'
 
 import { defaultOptions } from './options'
 
-const SMALL_SCREEN = 768
-
-const MAX_PARTICLES = 160
-const MIN_PARTICLES = 80
-
 const StyledParticles = styled(Particles)`
   position: fixed;
-
-  background-color: grey;
-
   width: 100%;
   height: 100%;
 `
 
 const ParticlesWrapper = () => {
-  // const size = useWindowSize()
-
-  // const [options, setOptions] = useState(defaultOptions)
-
-  // useLayoutEffect(() => {
-  //   const {
-  //     particles,
-  //     particles: {
-  //       number,
-  //       number: { value },
-  //     },
-  //   } = options
-
-  //   const isSmallScreen = size?.width && size.width <= SMALL_SCREEN
-
-  //   switch (isSmallScreen) {
-  //     case true:
-  //       const updatedParticles = {
-  //         ...particles,
-  //         number: { ...number, value: MAX_PARTICLES },
-  //       }
-
-  //       setOptions({
-  //         ...options,
-  //         particles: updatedParticles,
-  //       })
-  //       break
-  //     default:
-  //       if (value === MAX_PARTICLES) {
-  //         setOptions({
-  //           ...options,
-  //           particles: {
-  //             ...particles,
-  //             number: { ...number, value: MIN_PARTICLES },
-  //           },
-  //         })
-  //       }
-  //   }
-
-  //   // eslint-disable-next-line
-  // }, [size])
-
   return <StyledParticles id="tsparticles" {...{ options: defaultOptions }} />
 }
 
